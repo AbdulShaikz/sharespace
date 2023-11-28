403a36519d0c1792655b59358f5c1246c8a0e1d9
Abdul Shaik <starabdul100@gmail.com>
2023-11-28 11:49:12 +0530

    Corrected the import  of logo in the Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
+import logo from "../assets/logos/favicon.svg"
 
 // eslint-disable-next-line no-unused-vars
 function Logo({width= '100px', className=""}) {
   return (
     <div className={`flex justify-center md:justify-start items-center h-full ${className}`}>
         <img 
-            src="/src/assets/logos/favicon.svg"
+            src={logo}
             alt="logo" 
             // style={{ width: width }}
             className="sm:2/12 w-1/12"
