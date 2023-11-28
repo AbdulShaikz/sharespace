cce46b64560d92bf6fbe2cebf4a4ba72b16fe4de
Abdul Shaik <starabdul100@gmail.com>
2023-11-28 18:39:15 +0530

    Adding margin to the Spinner.jsx

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 function Spinner() {
   return (
     <div className="flex justify-center items-center h-full w-full">
-      <div className="animate-spin rounded-full border-blue-500 border-t-blue-500 border-t-8 h-12 w-12"></div>
+      <div className="animate-spin rounded-full border-blue-500 border-t-blue-500 border-t-8 h-12 w-12 m-8"></div>
     </div>
   );
 }
